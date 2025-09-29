0d528036409c09dfb415490bba7d00204ba5376a
PrathyushaHarishKumar-JB24771 <pharish1@umbc.edu>
2025-09-28 22:26:50 -0400

    Update enable_versioning.py

---
+++ enable_versioning.py
@@ -0,0 +1,11 @@
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'my-boto3-s3-bucket-prathyusha12345'
+
+# Enable versioning
+s3.put_bucket_versioning(
+    Bucket=bucket_name,
+    VersioningConfiguration={'Status': 'Enabled'}
+)
+print(f'Versioning enabled for bucket {bucket_name}!')
